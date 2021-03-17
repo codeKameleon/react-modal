0f92074190b0a9f3eb7ff7d97bbb6332f8a7f6ac
axel <tavormina.axel@gmail.com>
2021-03-17 14:52:52 +0100

    Modal component logic + basic styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
+import {useState} from 'react';
+import styled from 'styled-components';
+import GlobalFonts from './fonts/fonts';
+import { GlobalStyle } from './globalStyles';
+import { Modal } from './components/Modal';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Thumb = styled.figure`
+  font-family: 'Rubik Light', sans-serif;
+
+  img {
+    width: 300px;
+  }
+
+  figcaption {
+      h2 {
+        position: relative;
+
+        &::after {
+          position: absolute;
+          content : '';
+          bottom: -10px;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          background-color: #000;
+        }
+      }
+
+      h3 {
+        margin-top: 0.85rem;
+      }
+    }
+`
+
 function App() {
+  const [showModal, setShowModal] = useState(false)
+
+  const openModal = () => {
+    setShowModal(prev => !prev)
+  }
+
   return (
-    <div className="App">
-      <h1>Modal</h1>
-    </div>
+    <>
+      <GlobalFonts/>
+
+      <GlobalStyle/>
+
+      <Container>
+       <Thumb onClick={openModal}>
+         <img src="https://i.ibb.co/qrwDYv0/episode-1.jpg" target="_blank" alt="Vignette Boarderless Podcast Episode 1 avec Daniela"/>
+
+         <figcaption>
+           <h2>Episode 1</h2>
+           <h3>Avec Daniela</h3>
+         </figcaption>
+       </Thumb>
+
+       {showModal && <Modal showModal={showModal} setShowModal={setShowModal}/>}
+     </Container>
+    </>
   );
 }
 
